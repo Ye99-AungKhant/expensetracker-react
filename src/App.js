9cf5546f5ea724ba2212545d5efc6415f46980b7
Ye99-AungKhant <yeaungkhant077@gmail.com>
2023-01-19 21:28:17 +0630

    add expense list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Expense from './components/Expenses/Expense';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DummyExpense = [
+  { id: 1, title: "Car Insurance", amount: 200, date: new Date(2023, 1, 15) },
+  { id: 2, title: "Buy Watch", amount: 15, date: new Date(2023, 1, 17) },
+  { id: 3, title: "Present for girlfriend", amount: 150, date: new Date(2023, 1, 18) },
+  { id: 4, title: "Shoe", amount: 30, date: new Date(2023, 1, 5) },
+]
+
 const App = () => {
 
-  const expenses = [
-    { id: 1, title: "Car Insurance", amount: 200, date: new Date(2023, 1, 15) },
-    { id: 2, title: "Buy Watch", amount: 15, date: new Date(2023, 1, 17) },
-    { id: 3, title: "Present for girlfriend", amount: 150, date: new Date(2023, 1, 18) },
-    { id: 4, title: "Shoe", amount: 30, date: new Date(2023, 1, 5) },
-  ]
+  const [expenses, setExpenses] = useState(DummyExpense)
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpense) => {
+      return [expense, ...prevExpense]
+    })
   }
 
   return (
